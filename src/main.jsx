84cd342971b7639e78a982d07c67b65bfb7d68b2
Jimwel Dizon <jimweldizon.dev@gmail.com>
2022-12-03 10:35:07 +0800

    Fixed: comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+// libraries
+import { BrowserRouter } from 'react-router-dom'
+
 // components
 import App from './App'
 
@@ -9,7 +12,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@adminkit/core/dist/css/app.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/routes/UserRoute.jsx
+++ src/routes/UserRoute.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+
+// libraries
 import { Navigate, Outlet } from 'react-router-dom'
 
 const UserRoute = ({ auth }) => {
